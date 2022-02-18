36aca52882cfca73e33fb3e5ad08e0061fd4ccc0
Suryakiran7-2002 <ksuryakiranchary@gmail.com>
2022-02-18 14:57:12 +0530

    main update

--- app.py
+++ app.py
@@ -12,23 +12,35 @@ def education():
 
 @app.route('/education/10')
 def education10():
-    return render_template('error.html')
+    return render_template('10th.html')
 
 @app.route('/education/12')
 def education12():
-    return render_template('error.html')
+    return render_template('12th.html')
 
 @app.route('/education/uni')
 def education_uni():
-    return render_template('error.html')
+    return render_template('uni.html')
 
 @app.route('/certificates')
 def cerificates():
+    return render_template('certificates.html')
+
+@app.route('/certificates/courses')
+def cerificates_c():
+    return render_template('courses.html')
+
+@app.route('/certificates/hackathons')
+def cerificates_h():
+    return render_template('hackathons.html')
+
+@app.route('/certificates/workshops')
+def cerificates_w():
     return render_template('error.html')
 
 @app.route('/projects')
 def projects():
     return render_template('error.html')
 
-if __name__ == '__main__':  
-    app.run()
+
+app.run(debug=True)
